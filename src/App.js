32c6b0823779fd6ed436e3fa58fa6b319a41976e
yashodharanawaka <32728666+yashodharanawaka@users.noreply.github.com>
2020-11-03 07:33:15 +0530

    Added a navbar for the website

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Switch,
   Route,
   Redirect,
+  Link,
 } from "react-router-dom";
 
 import HomePage from "./Pages/HomePage";
@@ -13,6 +14,33 @@ import DramaListPage from "./Pages/DramaListPage";
 const App = () => {
   return (
     <Router>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link class="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/dramas">
+                Dramas
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/dramas" component={DramaListPage} />
